a7bb556011a2d1173b6bcd48327fc9ff57cf4073
Dimitrij Mijoski <dmjpp@hotmail.com>
2015-01-13 00:41:12 +0100

    Daden tocen algoritam. Dava greski vo randomTest zaradi numerickata nestabilnost i sproedbite so float.

--- luDecomposition/luDecomposition/lu.cu
+++ luDecomposition/luDecomposition/lu.cu
@@ -12,7 +12,7 @@
 using namespace std;
 
 template <typename T>
-__global__ void lu(T * matrica, T * lMat, T * uMat, int n)
+__global__ void luStaro(T * matrica, T * lMat, T * uMat, int n)
 {
 	int idx = blockDim.x * blockIdx.x + threadIdx.x;
 	
@@ -56,16 +56,74 @@ __global__ void lu(T * matrica, T * lMat, T * uMat, int n)
 	}
 }
 
+template <typename T>
+__global__ void lu1(T * L, T * U, int n, int i)
+{
+	//vo ovoj cekor presmetivame koef so koj kje mnozime gorna redica i kje gi eliminrame redicite pod nea
+	//idx ni e redica pocnuvanjki od A[i], i ni e kolona
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+	int redica = idx + i;
+	if (redica < n) {
+		L[redica*n + i] = U[redica*n + i] / U[i*n + i];
+	}
+}
+
+template <typename T>
+__global__ void lu2(T * L, T * U, int n, int i)
+{
+	//ovde idx ni e globalen indeks na niskata i globalen indeks vo matricata (row major) pocnuvajki od U[i+1][i]
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if (idx >= (n-i-1)*(n-i)) {
+		return;
+	}
+	//(n - i) sirina na redica sto ja obrabotivame. delime so sorinata i gi dodavame pomestuvanjeata t.e. offsetite
+	int redica = idx / (n - i) + i + 1;
+	int kolona = idx % (n - i) + i;
+	U[redica * n + kolona] -= L[redica * n + i] * U[i * n + kolona];
+}
 
 template <typename T>
 void printMatrix(T * mat, int red, int kol)
 {
-	for(int i = 0; i < red; i++) {
-		for(int j = 0; j < red; j++) {
+	for (int i = 0; i < red; i++) {
+		for (int j = 0; j < red; j++) {
 			cout << mat[i*kol + j] << ' ';
 		}
 		cout << endl;
 	}
+}
+
+template <typename T>
+void lu(T * d_L, T * d_U, int n)
+{
+	const int NUMTHREAD = 128;
+
+	float * L = new float[n*n];
+	float * U = new float[n*n];
+
+	/*cudaMemcpy(U, d_U, sizeof(U[0])*n*n, cudaMemcpyDeviceToHost);
+	printMatrix<float>(U, n, n);
+	cout << "======" << endl;*/
+
+	cudaMemset(d_L, 0, sizeof(T)*n*n);
+	for (int i = 0; i < n-1; i++) {
+		int lu1blocks = (n-i + NUMTHREAD - 1) / NUMTHREAD;
+		int lu2blocks = ((n-i-1)*(n-i) + NUMTHREAD - 1) / NUMTHREAD;
+		lu1<T> << <lu1blocks, NUMTHREAD >> >(d_L, d_U, n, i);
+		lu2<T> << <lu2blocks, NUMTHREAD >> >(d_L, d_U, n, i);
+
+		/*cudaMemcpy(L, d_L, sizeof(L[0])*n*n, cudaMemcpyDeviceToHost);
+		cudaMemcpy(U, d_U, sizeof(U[0])*n*n, cudaMemcpyDeviceToHost);
+
+		printMatrix<float>(L, n, n);
+		cout << '*' << endl;
+		printMatrix<float>(U, n, n);
+		cout << "======" << endl;*/
+	}
+	lu1<T> << <1, 1 >> >(d_L, d_U, n, n-1);
+
+	delete[] L; delete[] U;
 }
 
 void matMulCpuNaive(const float * A, const float * B, float * C, int redA, int kolARedB, int kolB)
@@ -102,16 +160,15 @@ bool test1()
 	float A[16] = { 1, 2, 3, 4, 4, 5, 6, 7, 7, 8, 10, 11, 11, 12, 13, 15 };
 	float L[16], U[16], novoA[16];
 	int n = 4;
-	float *d_A, *d_U, *d_L;
-	cudaMalloc(&d_A, sizeof(A));
+	float *d_U, *d_L;
 	cudaMalloc(&d_L, sizeof(L));
 	cudaMalloc(&d_U, sizeof(U));
-	cudaMemcpy(d_A, A, sizeof(A), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_U, d_A, sizeof(A), cudaMemcpyDeviceToDevice);
-	lu<float> <<< 3, 6 >>>(d_A, d_L, d_U, n); //javuva greska  za 6 bloka so po 3 niski <<<6,3>>>
+	cudaMemcpy(d_U, A, sizeof(A), cudaMemcpyHostToDevice);
+	lu<float>(d_L, d_U, n);
 	cudaMemcpy(L, d_L, sizeof(L), cudaMemcpyDeviceToHost);
 	cudaMemcpy(U, d_U, sizeof(U), cudaMemcpyDeviceToHost);
-
+	cudaFree(d_L);
+	cudaFree(d_U);
 	printMatrix<float>(L, 4, 4);
 	cout << '*' << endl;
 	printMatrix<float>(U, 4, 4);
@@ -132,7 +189,7 @@ bool test1()
 bool randomMatrixTest()
 {
 	srand(time(NULL));
-	int n = 10;
+	int n = 6;
 	float * A = new float[n*n];
 	float * L = new float[n*n];
 	float * U = new float[n*n];
@@ -142,14 +199,15 @@ bool randomMatrixTest()
 		A[i] = (float)(rand() % 1000);
 	}
 
-	float *d_A, *d_U, *d_L;
-	cudaMalloc(&d_A, sizeof(A[0])*n*n);
+	float *d_U, *d_L;
 	cudaMalloc(&d_L, sizeof(L[0])*n*n);
 	cudaMalloc(&d_U, sizeof(U[0])*n*n);
-	cudaMemcpy(d_A, A, sizeof(A[0])*n*n, cudaMemcpyHostToDevice);
-	lu<float> <<< (n*n+127)/128, 128 >>>(d_A, d_L, d_U, n);
+	cudaMemcpy(d_U, A, sizeof(A[0])*n*n, cudaMemcpyHostToDevice);
+	lu<float>(d_L, d_U, n);
 	cudaMemcpy(L, d_L, sizeof(L[0])*n*n, cudaMemcpyDeviceToHost);
 	cudaMemcpy(U, d_U, sizeof(U[0])*n*n, cudaMemcpyDeviceToHost);
+	cudaFree(d_L);
+	cudaFree(d_U);
 
 	printMatrix<float>(L, n, n);
 	cout << '*' << endl;
